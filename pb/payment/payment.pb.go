018848667a04dec48a4e1eddc934b3ff0a8d105a
jook25 <jook.noppadol@q-chang.com>
2026-02-17 12:39:59 +0700

    gen file

--- pb/payment/payment.pb.go
+++ pb/payment/payment.pb.go
@@ -26,6 +26,7 @@ type PayRequest struct {
 	OrderId       string                 `protobuf:"bytes,1,opt,name=order_id,json=orderId,proto3" json:"order_id,omitempty"`
 	Amount        float64                `protobuf:"fixed64,2,opt,name=amount,proto3" json:"amount,omitempty"`
 	Currency      string                 `protobuf:"bytes,3,opt,name=currency,proto3" json:"currency,omitempty"`
+	UserId        string                 `protobuf:"bytes,4,opt,name=user_id,json=userId,proto3" json:"user_id,omitempty"`
 	unknownFields protoimpl.UnknownFields
 	sizeCache     protoimpl.SizeCache
 }
@@ -77,6 +78,13 @@ func (x *PayRequest) GetAmount() float64 {
 func (x *PayRequest) GetCurrency() string {
 	if x != nil {
 		return x.Currency
+	}
+	return ""
+}
+
+func (x *PayRequest) GetUserId() string {
+	if x != nil {
+		return x.UserId
 	}
 	return ""
 }
@@ -137,12 +145,13 @@ var File_proto_payment_proto protoreflect.FileDescriptor
 
 const file_proto_payment_proto_rawDesc = "" +
 	"\n" +
-	"\x13proto/payment.proto\x12\apayment\"[\n" +
+	"\x13proto/payment.proto\x12\apayment\"t\n" +
 	"\n" +
 	"PayRequest\x12\x19\n" +
 	"\border_id\x18\x01 \x01(\tR\aorderId\x12\x16\n" +
 	"\x06amount\x18\x02 \x01(\x01R\x06amount\x12\x1a\n" +
-	"\bcurrency\x18\x03 \x01(\tR\bcurrency\"A\n" +
+	"\bcurrency\x18\x03 \x01(\tR\bcurrency\x12\x17\n" +
+	"\auser_id\x18\x04 \x01(\tR\x06userId\"A\n" +
 	"\vPayResponse\x12\x18\n" +
 	"\asuccess\x18\x01 \x01(\bR\asuccess\x12\x18\n" +
 	"\amessage\x18\x02 \x01(\tR\amessage2M\n" +
